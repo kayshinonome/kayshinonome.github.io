8c5e5510ba486f66640763eddfc642c6dfe4217a
Kay Shinonome <kayshinonome@protonmail.com>
2022-01-08 00:31:22 -0600

    website done

--- js/index.js
+++ js/index.js
@@ -2,8 +2,7 @@ let navbar = document.getElementsByClassName("navbar")[0];
 let info_box = document.getElementsByClassName("info-box")[0];
 
 navbar.addEventListener("click", (event) => {
-    if (event.target instanceof HTMLButtonElement) {
-        console.log(event.target.id);
+    if (event.target instanceof HTMLButtonElement && event.target.id != "") {
+        info_box.src = "html/" + event.target.id.toLowerCase() + ".html";
     }
-    info_box.src = "html/" + event.target.id.toLowerCase() + ".html";
 });
